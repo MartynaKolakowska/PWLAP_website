d7a82ef99fab276f299c692421100e11afa9e9c0
RootMD <Fosternmr@gmail.com>
2018-08-01 09:17:13 +0200

    php poprawka na maila

--- newsletter.php
+++ newsletter.php
@@ -2,10 +2,14 @@
 
 $emailError="";
 $email="";
-$host,$username,$password,$databasename = ""; // TODO
+//$host,$username,$password,$databasename = ""; // TODO: 1
 
-$connect=mysql_connect($host,$username,$password);
-$db=mysql_select_db($databasename);
+//$connect=mysql_connect($host,$username,$password); // TODO: 1
+//$db=mysql_select_db($databasename); // TODO: 1
+
+//$mailer="Kontakt@xmuch.media"; // TODO: 2
+//$subject="Subskrypcja"; // TODO: 2
+//$message="Dziękuję za Subskrypjcę! Od teraz będziesz z nami na bierząco!"; // TODO: 2
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	if(empty($_POST["email"])) {
@@ -15,7 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
   			$emailError = "Nieprawidłowy format!"; 
 		} else {
-			mysql_query("insert into dataBaseTable values('$email')");
+			//mysql_query("insert into dataBaseTable values('$email')"); // TODO: 1
+			//mail($mailer, $subject, $message) // TODO: 2
 			$emailError = "Będziemy w kontakcie!";
 		}
 	}
